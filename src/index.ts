2c22210be26f08f017f6dcb09c58ee5fd813c7a2
alexwebdev05 <alexfriasangulo@gmail.com@>
2024-12-24 14:13:28 +0100

    shared port

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
-const PORT = process.env.PORT;
+const PORT = process.env.API_PORT;
 
 // Habilitar solicitudes HTTP desde el cliente
 app.use(cors());
