857d8d9ea7790a8c2f9e58253026e32dd0cfc1ed
alexwebdev05 <alexfriasangulo@gmail.com@>
2024-12-24 12:55:15 +0100

    Port change

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-import './http';
-import './websocket';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { Server as WebSocketServer } from 'ws';
+import * as dotenv from 'dotenv';
+import router from './http/router';
+import socketRouter from './sockets/router';
+import http from 'http';
+
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app);
+const PORT = process.env.HTTP;
+
+// Habilitar solicitudes HTTP desde el cliente
+app.use(cors());
+
+// Convertir los datos recibidos a JSON
+app.use(bodyParser.json());
+
+// Rutas HTTP
+app.use('/api', router);
+
+// Configurar WebSocket en el mismo puerto
+const wss = new WebSocketServer({ server: httpServer });
+socketRouter(wss);
+
+// Iniciar el servidor HTTP y WebSocket
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`WebSocket server is running on ws://localhost:${PORT}`);
+});
