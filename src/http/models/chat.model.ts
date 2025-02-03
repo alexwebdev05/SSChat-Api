1fd9cb4bef2e2eb33ca252c4a8c1fe0e519b22fa
alexwebdev05 <alexfriasangulo@gmail.com@>
2025-02-03 18:38:04 +0100

    newChat update

--- src/http/models/chat.model.ts
+++ src/http/models/chat.model.ts
@@ -55,19 +55,32 @@ export class ChatModel {
     static async getChats(data: Omit<IChat, 'user2'> & {user: string}) {
         const client = await dbConnect();
 
+        // Get chats
         try {
-            const username = data.user1; 
+
+            // Set username
+            const username = data.user1;
+
+            // Get chats query
             const response = await client.query(
                 'SELECT * FROM chats WHERE user1 = $1 OR user2 = $2',
                 [username, username]
             );
+
+            // Handle errors
             if (response.rows.length === 0) {
                 return { message: 'No chats found' };
             }
+
+            // Return result
             return response.rows;
+        
+        // Handle errors
         } catch(error) {
             console.log('[ SERVER ] Failed to get chats at model: ' + error);
             return { message: 'Error retrieving chats' };
+
+        // Release client
         } finally {
             try {
                 client.release();
